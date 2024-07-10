1a86c4575a620679904061db507cd1879a60d0d2
Filip Sakala <filip.sakala@vacuumlabs.com>
2024-07-10 17:25:14 +0200

    feat: style loan calculation

--- src/app/components/LoanCalculator.tsx
+++ src/app/components/LoanCalculator.tsx
@@ -1,9 +1,33 @@
+import Image from "next/image";
 import styles from "./LoanCalculator.module.css";
 
 const LoanCalculator = () => {
   return (
-    <div className={styles.wrapper}>
-      <h2 className={styles.title}>Express půjčku schválíme do 5 minut</h2>
+    <div className={styles.calculator}>
+      <div className={styles.inputsWrapper}>
+        <h2 className={styles.title}>Express půjčku schválíme do 5 minut</h2>
+      </div>
+      <div className={styles.calculationWrapper}>
+        <div className={styles.monthlyPaymentWrapper}>
+          <div className={styles.monthlyPaymentTitle}>Měsíčně zaplatíte</div>
+          <div className={styles.monthlyPaymentAmount}>1 358 Kč</div>
+        </div>
+        <div className={styles.monthlyPaymentWrapper}>
+          <div>Pokračovat...</div>
+          <div className={styles.calculationActionCall}>
+            <Image
+              className={styles.calculationActionCallIcon}
+              src="https://api.iconify.design/material-symbols:call-outline-sharp.svg"
+              alt="phone"
+              width="15"
+              height="15"
+            />
+            <span className={styles.calculationActionCallTitle}>
+              nebo Vám zavoláme
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
