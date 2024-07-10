afde5d3c4fc6612f78bb325540fe1540b29cef25
Filip Sakala <filip.sakala@vacuumlabs.com>
2024-07-10 16:51:39 +0200

    feat: arrange page and component layout

---
+++ src/app/components/LoanCalculator.tsx
@@ -0,0 +1,11 @@
+import styles from "./LoanCalculator.module.css";
+
+const LoanCalculator = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>Express půjčku schválíme do 5 minut</h2>
+    </div>
+  );
+};
+
+export default LoanCalculator;
